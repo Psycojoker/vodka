7acb73d7e75a63f116c8736724cb2bd855947c29
Laurent Peuch <cortex@worlddomination.be>
2013-01-29 23:06:15 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='vodka',
+      version='0.1',
+      description='Information extractor from OpenERP code',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/vodka',
+      install_requires=[],
+      packages=[],
+      license= 'GPLv3+',
+      scripts=[],
+      py_modules=['vodka'],
+      keywords='openerp ast',
+     )
